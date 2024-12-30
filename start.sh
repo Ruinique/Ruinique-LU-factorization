d07c2ac5f6fa543fc976ad4ee4fa5dd7023a06d0
Ruinique <2284818584@qq.com>
2024-12-30 17:18:31 +0800

    稍微修改一下

---
+++ start.sh
@@ -0,0 +1,9 @@
+###
+ # @Descripttion: 
+ # @Author: Ruinique
+ # @version: 
+ # @Date: 2024-12-30 17:17:25
+ # @LastEditors: Ruinique
+ # @LastEditTime: 2024-12-30 17:18:07
+### 
+cmake --build build && ./build/lu-factorization-gpu
