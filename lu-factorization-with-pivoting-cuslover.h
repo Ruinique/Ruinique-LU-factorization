76edbe0912f6994eace85448f57d0cf1ac064c33
Ruinique <2284818584@qq.com>
2024-12-31 10:04:10 +0800

    加一下单精度的场景

--- lu-factorization-with-pivoting-cuslover.h
+++ lu-factorization-with-pivoting-cuslover.h
@@ -87,6 +87,68 @@ float call_cusolver_to_lu_factorization_double(double *matrix, int64_t m,
     return time;
 }
 
+float call_cusolver_to_lu_factorization_float(float *matrix, int64_t m,
+                                              int64_t n, int64_t lda,
+                                              int64_t *pivot) {
+    float *A_gpu;
+    cudaMalloc((void **)&A_gpu, m * n * sizeof(double));
+    cudaMemcpy(A_gpu, matrix, m * n * sizeof(double), cudaMemcpyHostToDevice);
+    // create cusolver handle
+    cusolverDnHandle_t cusolver_lu_factorization_handler;
+    cusolverDnCreate(&cusolver_lu_factorization_handler);
+    // get size of working buffer
+    size_t device_working_buffer_size;
+    size_t host_working_buffer_size;
+    cusolverDnXgetrf_bufferSize(cusolver_lu_factorization_handler, nullptr, m,
+                                n, CUDA_R_32F, matrix, lda, CUDA_R_32F,
+                                &device_working_buffer_size,
+                                &host_working_buffer_size);    
+    // allocate related resource
+    int *info_lu_factorazation_gpu;
+    cudaMalloc((void **)&info_lu_factorazation_gpu, sizeof(int));
+    void *host_working_buffer = new char[host_working_buffer_size];
+    void *device_working_buffer;
+    cudaMalloc(&device_working_buffer, device_working_buffer_size);
+    float *matrix_gpu;
+    cudaMalloc((void **)&matrix_gpu, m * n * sizeof(float));
+    cudaMemcpy(matrix_gpu, matrix, m * n * sizeof(float),
+               cudaMemcpyHostToDevice);
+    int64_t *pivot_gpu;
+    cudaMalloc((void **)&pivot_gpu, std::min(m, n) * sizeof(int64_t));
+    Ruinique_CUDA_Timer<std::function<void()>> timer;
+    float time = timer.time_function([&]() {
+        // call cusolver
+        cusolverDnXgetrf(cusolver_lu_factorization_handler, nullptr, m, n,
+                         CUDA_R_64F, matrix_gpu, lda, pivot_gpu, CUDA_R_64F,
+                         device_working_buffer, device_working_buffer_size,
+                         host_working_buffer, host_working_buffer_size,
+                         info_lu_factorazation_gpu);
+    });
+    printf("cusolverDnXgetrf time: %f ms\n", time);
+    // check res
+    int info_lu_factorazation_cpu = 0;
+    cudaMemcpy(&info_lu_factorazation_cpu, info_lu_factorazation_gpu,
+               sizeof(int), cudaMemcpyDeviceToHost);
+    cudaFree(info_lu_factorazation_gpu);
+    if (info_lu_factorazation_cpu != 0) {
+        throw std::runtime_error("LU factorization failed with non-zero info.");
+    }
+    cudaMemcpy(matrix, matrix_gpu, m * n * sizeof(float),
+               cudaMemcpyDeviceToHost);
+    // transport res to host
+    // checkAns4Cusolver(matrix, matrix_gpu, m, n, pivot, pivot_gpu);
+    // free resource
+    cudaFree(matrix_gpu);
+    cudaFree(pivot_gpu);
+    delete[] (char *)host_working_buffer;
+    cudaFree(device_working_buffer);
+    cudaFree(A_gpu);
+    cusolverDnDestroy(cusolver_lu_factorization_handler);
+    // return time for test
+    return time;
+}
+
+
 void checkAns4Cusolver(double *matrix, double *matrix_gpu, int64_t &m,
                        int64_t &n, int64_t *pivot, int64_t *pivot_gpu) {
     print_matrix_double(matrix, m, n);
