e5e60ad07f1b6c0ea4089cf23d3fd5e544b196e2
Ruinique <2284818584@qq.com>
2025-01-02 15:22:05 +0800

    增大数据量

--- test-gpu.cu
+++ test-gpu.cu
@@ -4,14 +4,14 @@
  * @version: 
  * @Date: 2024-11-21 13:55:45
  * @LastEditors: Ruinique
- * @LastEditTime: 2025-01-02 15:06:47
+ * @LastEditTime: 2025-01-02 15:21:18
  */
 #include "common.h"
 #include "lu-factorization-with-pivoting-cuslover.h"
 
 size_t calculate_memory_usage(int64_t m, int64_t n, int64_t lda);
 
-const int testSize[13] = {8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768};
+const int testSize[14] = {8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 49152};
 
 int main() {
     // 首先调用 5 轮 size = 1024 进行 warm up
@@ -23,8 +23,8 @@ int main() {
         delete[] matrix;
         delete[] pivot;
     }
-    float time[13];
-    size_t memory[13];
+    float time[14];
+    size_t memory[14];
     for (int i = 0; i < 13; i++) {
         int size = testSize[i];
         size_t memory_usage = calculate_memory_usage(size, size, size);
@@ -40,13 +40,13 @@ int main() {
         delete[] matrix;
         delete[] pivot;
     }
-    for (int i = 0; i < 13; i++) {
+    for (int i = 0; i < 14; i++) {
         printf("Time for %d x %d matrix: %f\n", testSize[i], testSize[i], time[i]);
     }
     // 将结果写入 CSV 文件
     std::ofstream csv_file("lu_factorization_times.csv");
     csv_file << "Matrix Size,Time (ms),Memory Usage (bytes)\n";
-    for (int i = 0; i < 13; i++) {
+    for (int i = 0; i < 14; i++) {
         csv_file << testSize[i] << "x" << testSize[i] << "," << time[i] << "," << memory[i] << "\n";
     }
     csv_file.close();
